659c9c97ca62b949ece017acc9542a5d9a1f8507
Amritha <mini26644@gmail.com>
2026-06-14 13:13:59 +0530

    first update

---
+++ dsa/ex1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+class Product
+{
+  public int ProductId {get; set;}
+  public string ProductName {get; set;}
+  public int Quantity {get; set;}
+  public double Price {get; set;}
+  public Product(int productId, string productName, int quantity, double price)
+  {
+    ProductId= productId;
+    ProductName = productName;
+    Quantity = quantity;
+    Price = price;
+  }
+  public override string ToString ()
+  {
+    return $"id : {ProductId}, product name : {ProductName}, quantity : {Quantity}, price: {Price}";
+  }
+}
+class Inventory
+{
+  private Dictionary<int, Product> products = new Dictionary<int, Product>();
+  public void AddProduct (Product product)
+  {
+    products[product.ProductId] = product;
+    Console.Write("added");
+  }
+
+  public void UpdateProduct (int id, string name, int quantity, double price)
+  {
+    if (products.ContainsKey(id))
+    {
+      products[id].ProductName = name;
+      products[id].Quantity = quantity;
+      products[id].Price = price;
+      Console.WriteLine("updated.");
+    } else
+    {
+      Console.WriteLine("not found.");
+    }
+  }
+
+  public void DeleteProduct (int id)
+  {
+    if(products.Remove(id))
+    {
+      Console.WriteLine("deleted");
+    } else
+    {
+      Console.WriteLine("not found");
+    }
+  }
+  public void Display()
+  {
+    foreach (Product product in products.Values) {
+      Console.WriteLine(product);
+    }
+  }
+}
+
+class Program
+{
+  static void Main()
+  {
+    Inventory inventory = new Inventory();
+    inventory.AddProduct(new Product (101, "laptop", 20, 55000));
+    inventory.AddProduct(new Product (102, "mouse", 50, 500));
+    Console.Write("data:");
+    inventory.Display();
+    inventory.UpdateProduct(101, "gaming laptop", 15, 65000);
+    Console.Write("updated data:");
+    inventory.Display();
+    inventory.DeleteProduct(102);
+    inventory.Display();
+  }
+}
---
+++ dsa/ex2/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+class Product
+{
+  public int ProductId {get; set;}
+  public string ProductName {get; set;}
+  public string Category {get; set;}
+
+  public Product (int productId, string productName, string category)
+  {
+    ProductId = productId;
+    ProductName = productName;
+    Category = category;
+  }
+}
+
+class Program
+{
+  static int Linear(Product[] products, int TargetID)
+  {
+    for(int i=0; i<products.Length; i++)
+    {
+      if(products[i].ProductId==TargetID)
+      return i;
+    }
+    return -1;
+  }
+
+  static int Binary(Product[] products, int TargetID)
+  {
+    int left=0;
+    int right=products.Length-1;
+
+    while(left<=right)
+    {
+      int mid = (left+right)/2;
+      if(products[mid].ProductId==TargetID)
+      {
+        return mid;
+      } else if(products[mid].ProductId<TargetID)
+      {
+        left=mid+1;
+      } else
+      {
+        right=mid-1;
+      }
+    }
+    return -1;
+  }
+
+  static void Main ()
+  {
+    Product[] product=
+    {
+      new Product(105, "Laptop", "Electronics"),
+      new Product(102, "Mouse", "Electronics"),
+      new Product(108, "Shoes", "Fashion"),
+      new Product(101, "Book", "Education"),
+      new Product(110, "Watch", "Accessories")
+    };
+
+    Product[] sortedproduct =
+    {
+      new Product(101, "Book", "Education"),
+      new Product(102, "Mouse", "Electronics"),
+      new Product(105, "Laptop", "Electronics"),
+      new Product(108, "Shoes", "Fashion"),
+      new Product(110, "Watch", "Accessories")
+    };
+
+    int searchID =108;
+
+    int linearResult = Linear(product, searchID);
+    if(linearResult!=-1)
+      Console.WriteLine("found at: " + product[linearResult].ProductName);
+    else
+      Console.WriteLine("not found");
+
+    int binaryResult = Binary(sortedproduct, searchID);
+    if(binaryResult!=-1)
+      Console.WriteLine("found at: " + sortedproduct[binaryResult].ProductName);
+    else
+      Console.WriteLine("not found");
+  }
+}

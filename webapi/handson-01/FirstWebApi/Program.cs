d1407618ea4fe2cd5af3741dcb749332ee53caa1
Amritha <mini26644@gmail.com>
2026-07-14 22:53:39 +0530

    modified and added handson 02

--- webapi/handson-01/FirstWebApi/Program.cs
+++ webapi/handson-01/FirstWebApi/Program.cs
@@ -4,14 +4,42 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+    {
+        Title = "Swagger Demo",
+        Version = "v1",
+        Description = "TBD",
+
+        Contact = new Microsoft.OpenApi.OpenApiContact
+        {
+            Name = "John Doe",
+            Email = "john@xyzmail.com",
+            Url = new Uri("https://www.example.com")
+        },
+
+        License = new Microsoft.OpenApi.OpenApiLicense
+        {
+            Name = "License Terms",
+            Url = new Uri("https://www.example.com")
+        }
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger Demo");
+    });
 }
 
 app.UseHttpsRedirection();

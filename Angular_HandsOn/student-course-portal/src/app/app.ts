26d3c831296e92983e2831a29bad3725231fbb48
Amritha <mini26644@gmail.com>
2026-07-25 12:56:33 +0530

    completed angular handson task 2

--- Angular_HandsOn/student-course-portal/src/app/app.ts
+++ Angular_HandsOn/student-course-portal/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Home } from './pages/home/home';
+import { CourseList } from './pages/course-list/course-list';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Home],
+  imports: [RouterOutlet, Header, Home, CourseList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
+++ Angular_HandsOn/student-course-portal/src/app/pages/course-list/course-list.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseCard } from '../../components/course-card/course-card';
 
 @Component({
   selector: 'app-course-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CourseCard],
   templateUrl: './course-list.html',
   styleUrl: './course-list.css',
 })
-export class CourseList {}
+export class CourseList {
+
+  courses = [
+  {
+    id: 1,
+    name: 'Angular',
+    code: 'ANG101',
+    credits: 4
+  },
+  {
+    id: 2,
+    name: 'C#',
+    code: 'CS102',
+    credits: 3
+  },
+  {
+    id: 3,
+    name: 'SQL',
+    code: 'SQL103',
+    credits: 2
+  },
+  {
+    id: 4,
+    name: 'ASP.NET Core',
+    code: 'ASP104',
+    credits: 4
+  },
+  {
+    id: 5,
+    name: 'JavaScript',
+    code: 'JS105',
+    credits: 3
+  }
+];
+
+selectedCourseId: number | null = null;
+
+onEnroll(courseId: number) {
+  console.log('Enrolling in course: ' + courseId);
+  this.selectedCourseId = courseId;
+}
+
+}

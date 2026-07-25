c88a904b73a08d125ebf0bcaf7322c7f653f42f5
Amritha <mini26644@gmail.com>
2026-07-25 10:10:44 +0530

    angular handson task 1 completed

---
+++ Angular_HandsOn/student-course-portal/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { Header } from './components/header/header';
+import { Home } from './pages/home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [Header, Home],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('student-course-portal');
+}

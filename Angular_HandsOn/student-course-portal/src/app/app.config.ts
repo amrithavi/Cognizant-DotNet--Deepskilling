bd5a83963caf6234ac56c17bad9064e1605c3c6d
Amritha <mini26644@gmail.com>
2026-07-28 22:11:07 +0530

    update

--- Angular_HandsOn/student-course-portal/src/app/app.config.ts
+++ Angular_HandsOn/student-course-portal/src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideEffects } from '@ngrx/effects';
+import { CourseEffects } from './store/course/course.effects';
 
 import { courseReducer } from './store/course/course.reducer';
 
@@ -17,6 +19,10 @@ export const appConfig: ApplicationConfig = {
     provideStore({
     courses: courseReducer
     }),
+
+    provideEffects([
+      CourseEffects
+    ]),
 
     provideStoreDevtools({
       maxAge: 25
